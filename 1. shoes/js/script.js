62b1880cce46e09a4bd29a4de9657a3bf71be0b7
ademagar <ad3magar@gmail.com>
2023-07-16 16:36:39 +0300

    re-init

--- 1. shoes/js/script.js
+++
@@ -1,2 +0,0 @@
-// import images as relative image path won't work with vite/vercel.
-/*  import name from '../assets/name.dosyauzantisi'   */
---
+++ 1.shoes/js/script.js
@@ -0,0 +1,2 @@
+// import images as relative image path won't work with vite/vercel.
+/*  import name from '../assets/name.dosyauzantisi'   */
